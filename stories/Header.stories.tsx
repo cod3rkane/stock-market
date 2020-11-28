c189b97acd39065e0d107e3554b9e977ab7f9c6f
cod3rkane <jcesar.developer@gmail.com>
2020-11-28 16:22:35 -0500

    code improvements

---
+++ stories/Header.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import Header from '@/components/Header'
+
+storiesOf('Header', module).add('Default', () => {
+  return <Header />
+})
