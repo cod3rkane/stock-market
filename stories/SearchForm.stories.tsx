767121881cc1f4b7ce5ad28b011f5cb50adf47a1
cod3rkane <jcesar.developer@gmail.com>
2020-11-29 17:38:00 -0500

    clean code

--- stories/SearchForm.stories.tsx
+++ stories/SearchForm.stories.tsx
@@ -3,5 +3,12 @@ import { storiesOf } from '@storybook/react'
 import SearchForm from '@/components/molecules/SearchForm'
 
 storiesOf('SearchForm', module).add('Default', () => {
-  return <SearchForm />
+  return (
+    <SearchForm
+      onClickMedia={() => 1}
+      onClickStock={() => 1}
+      selectedMedia={null}
+      selectedStock={null}
+    />
+  )
 })
